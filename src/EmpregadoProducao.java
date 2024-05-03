533fce2501cc2dcf9465728e3dc6ddf306bb1418
Gabriel Neves <gabrielnevesferreira08@gmail.com>
2024-05-03 18:04:34 -0300

    first commit

---
+++ src/EmpregadoProducao.java
@@ -0,0 +1,53 @@
+public class EmpregadoProducao extends Empregado {
+
+    private double valorUnitarioPeca;
+    private int qunatidadePecasProduzidas;
+    
+    //CONSTRUTOR
+    public EmpregadoProducao (
+        String Nome,
+        String Sobrenome,
+        String CPF,
+        int    Day,
+        int    Month,
+        int    Year,
+        double  valorUnitarioPeca,
+        int    quantidadePecasProduzidas)
+    {
+        super(Nome, Sobrenome, CPF, Day, Month, Year);
+        setValorUnitario(valorUnitarioPeca);
+        getQuantidadePecasProduzidas(quantidadePecasProduzidas);
+    }
+    
+    public double getValorUnitario() {
+        return valorUnitarioPeca;
+    }
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitarioPeca = valorUnitario;
+    }
+    public int getQuantidadePecasProduzidas() {
+        return qunatidadePecasProduzidas;
+    }
+    public void getQuantidadePecasProduzidas(int qunatidadePecasProduzidas) {
+        this.qunatidadePecasProduzidas = qunatidadePecasProduzidas;
+    }
+    
+    @Override
+    public double vencimentos() {
+        return getQuantidadePecasProduzidas() * getValorUnitario();
+    }
+
+    public String toString() {
+        return String.format( "%s: %s\n%s: %d; %s: %.2f",    
+         "Empregado Producao", super.toString(),              
+         "Quantidade de peças produzidas", getQuantidadePecasProduzidas(),                       
+         "Valor Unitário", getValorUnitario() );
+    }
+
+    //MÉTODO PARA CALCULAR O SALÁRIO EM FUNÇÃO DE QUANTIDADE DE 'PEÇAS PRODUZIDAS' E 'PEÇAS VENDIDAS'
+    public double calcularSalario(int quantiaPecas, double valorUnitario) {
+
+        double salario = quantiaPecas * valorUnitario;
+        return salario;
+    }
+}
---
+++ src/LabMain.java
@@ -0,0 +1,24 @@
+// ----------------------------------------
+// Prof.: CARLOS - FATEC S. J. Rio Preto
+// Disc.: IED-001 - ESTRUTURAS DE DADOS
+// Lab. : POLIMORFISMO e INTERFACES em JAVA
+// Ano/S: 2024/1S
+// ----------------------------------------
+
+// classe que contém o 'main'
+// --------------------------
+
+import java.time.LocalDate;
+
+public class LabMain {
+
+    public static void main(String[] args) {
+        
+        // instanciação de um objeto de classe 'CoordenadorExecucao'
+        CoordenadorExecucao coord = new CoordenadorExecucao();
+        
+        // acionamento do método de 'entrada' de CoordenadorExecucao'
+        coord.executaTudo();
+
+    }
+}
